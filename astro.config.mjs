c36da33735403696a0a22b94cbebe7067cd03770
Sevi <s.sager91@gmail.com>
2026-05-02 17:49:50 +0200

    Fix CSS: unicode quotes properly escaped

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+// Hinweis: Sitemap-Integration wird später wieder eingefügt,
+// sobald die echte Domain verbunden ist.
 export default defineConfig({
   site: 'https://sinnorientiert.de',
 });
